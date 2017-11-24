eb9ea255af0daea35885757502c376dcec7de04b
caorenxxy <caorenxxy@gmail.com>
2017-11-24 09:22:58 +0800

    cm

--- iso.m
+++ iso.m
@@ -7,3 +7,13 @@ options.dims = 2;
 [Y, ~, ~] = Isomap(D, 'k', 5, options);
 Z = Y.coords{1,1}';
 scatter(Z(:,1),Z(:,2),90,tt,'.');
+
+
+clc;
+clear;
+[trainSet,tt]=swiss_roll2(1500);
+trainSet = trainSet';
+Y = lle(trainSet, 12, 2);
+Y = Y';
+figure;
+scatter(Y(:,1),Y(:,2),90,tt,'.');
